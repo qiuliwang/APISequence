3225fc1bce9e66668b7c7b79041cbaa61e2480a4
wangqiuli1012 <418674878@qq.com>
2017-03-05 10:22:20 +0800

    os

--- Eclipse/src/JdkCore/start.java
+++ Eclipse/src/JdkCore/start.java
@@ -3,12 +3,12 @@ package JdkCore;
 public class start {
 	public static void main(String args[])
 	{
+		System.out.println(System.getProperty("os.name"));
 		String path = "/Users/WangQL/Documents/git/APISequence/Eclipse/src/JdkCore/testContent.java";
 		//git test
-		ReadFile rf = new ReadFile();
-		rf.setPath(path);
-		String content = rf.getContent();
-		//System.out.println(content.toString());
-		ASTAna asa = new ASTAna(content);
+		//ReadFile rf = new ReadFile();
+		//rf.setPath(path);
+		//String content = rf.getContent();
+		//ASTAna asa = new ASTAna(content);
 	}
 }
