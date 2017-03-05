3344f38513929b4a61e194fff633e57a6d879a68
wangqiuli1012 <418674878@qq.com>
2017-03-05 09:32:03 +0800

    test

--- Eclipse/src/JdkCore/start.java
+++ Eclipse/src/JdkCore/start.java
@@ -4,6 +4,7 @@ public class start {
 	public static void main(String args[])
 	{
 		String path = "/Users/WangQL/Documents/git/APISequence/Eclipse/src/JdkCore/testContent.java";
+		//git test
 		ReadFile rf = new ReadFile();
 		rf.setPath(path);
 		String content = rf.getContent();
